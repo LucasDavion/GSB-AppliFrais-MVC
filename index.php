93ca5d9d0cb372084a30a9ad1caff7941f9af856
dupontL64 <45233269+dupontL64@users.noreply.github.com>
2019-11-07 11:37:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
+//création d'une session
 session_start();
+//on inclue le fichier fct.inc.php
 require_once("include/fct.inc.php");
-require_once ("modeles/m_pdogsb.php");
+//on inclue le fichier m_pdogsb.php
+require_once("modeles/m_pdogsb.php");
+//on inclue le v_entete.php
 include("vues/v_entete.php") ;
+//création d'un objet de la classe PdoGsb
 $pdo = PdoGsb::getPdoGsb();
 $estConnecte = estConnecte();
 if(!isset($_REQUEST['uc']) || !$estConnecte){
