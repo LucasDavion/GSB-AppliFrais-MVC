a9b2f4aa1e96ead1ff490517c6fceb9e5e6da110
Liankir <45237185+Liankir@users.noreply.github.com>
2019-11-12 08:23:11 +0100

    commentaire

--- controleurs/c_gererFrais.php
+++ controleurs/c_gererFrais.php
@@ -1,17 +1,23 @@
 <?php
+// inclusion de la vue du sommaire
 include("vues/v_sommaire.php");
+// intitalisation des variable
 $idVisiteur = $_SESSION['idVisiteur'];
 $mois = getMois(date("d/m/Y"));
 $numAnnee =substr( $mois,0,4);
 $numMois =substr( $mois,4,2);
 $action = $_REQUEST['action'];
+
+// selection des diferante action a realiser en fonction de se qui se trouve dans $action et par exention dans $_request[action]
 switch($action){
+	// si action contrien saisireFrais alors, si c'est le premier frais du mois on cree une nouvelle lignes de frais
 	case 'saisirFrais':{
 		if($pdo->estPremierFraisMois($idVisiteur,$mois)){
 			$pdo->creeNouvellesLignesFrais($idVisiteur,$mois);
 		}
 		break;
 	}
+	// si action contien validerMajFraisForfait on ajoute a $lesFrais le contenue de $_request['lesFrais'] et si lesQteFraisValides contien qqch on créer u obj pdo avec majFraisForfait
 	case 'validerMajFraisForfait':{
 		$lesFrais = $_REQUEST['lesFrais'];
 		if(lesQteFraisValides($lesFrais)){
@@ -23,6 +29,7 @@ switch($action){
 		}
 	  break;
 	}
+	// si action contien validerCrationFrais on intitalise des variable avec le $_get si il y a des erreur on appelle v_erreurs.php
 	case 'validerCreationFrais':{
 		$dateFrais = $_REQUEST['dateFrais'];
 		$libelle = $_REQUEST['libelle'];
@@ -42,6 +49,8 @@ switch($action){
 		break;
 	}
 }
+
+// on recuperre les frais et on inclut les vue des frais
 $lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idVisiteur,$mois);
 $lesFraisForfait= $pdo->getLesFraisForfait($idVisiteur,$mois);
 include("vues/v_listeFraisForfait.php");
--- vues/v_accueil.php
+++ vues/v_accueil.php
@@ -1,3 +1,4 @@
-﻿<div id="contenu">
+﻿<!-- creation d'une div contenant une titre souhaitant la bienvenue au utilisateur de l'application -->
+<div id="contenu">
 	<h2>Bienvenue dans l'application de gestion des frais</h2>
 </div>
