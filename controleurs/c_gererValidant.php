4297c1aba11747ade46c68d85e41e4a547c4774b
dupontL64 <45233269+dupontL64@users.noreply.github.com>
2019-11-19 08:48:19 +0100

    Create c_gererValidant.php

---
+++ controleurs/c_gererValidant.php
@@ -0,0 +1,64 @@
+<?php
+// inclusion de la vue du sommaire
+include("vues/v_sommaire.php");
+// intitalisation des variable
+$idVisiteur = $_SESSION['idVisiteur'];
+
+$action = $_REQUEST['action'];
+
+// selection des diferante action a realiser en fonction de se qui se trouve dans $action et par exention dans $_request[action]
+switch($action){
+	// si action contrien saisireFrais alors, si c'est le premier frais du mois on cree une nouvelle lignes de frais
+	case 'selectionnerFicheFrais':{
+		if($pdo->estPremierFraisMois($idVisiteur,$mois)){
+			$pdo->creeNouvellesLignesFrais($idVisiteur,$mois);
+		}
+		break;
+	}
+	// si action contien validerMajFraisForfait on ajoute a $lesFrais le contenue de $_request['lesFrais'] et si lesQteFraisValides contien qqch on créer u obj pdo avec majFraisForfait
+	case 'afficherFF+FHF':{
+		$lesFrais = $_REQUEST['lesFrais'];
+		if(lesQteFraisValides($lesFrais)){
+	  	 	$pdo->majFraisForfait($idVisiteur,$mois,$lesFrais);
+		}
+		else{
+			ajouterErreur("Les valeurs des frais doivent être numériques");
+			include("vues/v_erreurs.php");
+		}
+	  break;
+	}
+	// si action contien validerCrationFrais on intitalise des variable avec le $_get si il y a des erreur on appelle v_erreurs.php
+	case 'validerFF':{
+		$dateFrais = $_REQUEST['dateFrais'];
+		$lstLibelle = $_REQUEST['lstLibelle'];
+		$montant = $_REQUEST['montant'];
+		valideInfosFrais($dateFrais,$lstLibelle,$montant);
+		if (nbErreurs() != 0){
+			include("vues/v_erreurs.php");
+		}
+		else{
+			$pdo->creeNouveauFraisHorsForfait($idVisiteur,$mois,$lstLibelle,$dateFrais,$montant);
+		}
+		break;
+	}
+	case 'supprimerFHF':{
+		$idFrais = $_REQUEST['idFrais'];
+	    $pdo->supprimerFraisHorsForfait($idFrais);
+		break;
+    }
+    case 'validerFicheFrais':{
+		$idFrais = $_REQUEST['idFrais'];
+	    $pdo->supprimerFraisHorsForfait($idFrais);
+		break;
+    }
+    
+}
+
+// on recuperre les frais et on inclut les vue des frais
+$lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idVisiteur,$mois);
+$lesFraisForfait= $pdo->getLesFraisForfait($idVisiteur,$mois);
+$lesTypesFraisHorsForfait = $pdo->getLesTypesFraisHorsForfait();
+include("vues/v_listeFraisForfait.php");
+include("vues/v_listeFraisHorsForfait.php");
+
+?>
