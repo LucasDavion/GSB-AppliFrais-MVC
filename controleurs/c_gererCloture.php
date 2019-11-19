7d423be80177dd2a1e0e5bb3db789e1af9deb0f8
Lucas <45062643+LucasDavion@users.noreply.github.com>
2019-11-19 08:25:47 +0100

    Update c_gererCloture.php

---
+++ controleurs/c_gererCloture.php
@@ -0,0 +1,25 @@
+<?php
+//On inclue v_sommaire.php
+include("vues/v_sommaire.php");
+//On récupère l'action selectionner
+$action = $_REQUEST['action'];
+//On récupère l'id du visiteur
+$idVisiteur = $_SESSION['idVisiteur'];
+
+$action = $_REQUEST['action'];
+
+switch ($action) {
+    case 'affListNonCL':
+    $lesFraisHorsForfait = $pdo->getLesFichesFraisMoisPrecedentNonCloturer();
+    include("vues/v_listeFichesFrais.php");
+        break;
+
+    case 'CLFicheNonCL':
+    # code...
+    break;
+    
+    default:
+        # code...
+        break;
+}
+?>
